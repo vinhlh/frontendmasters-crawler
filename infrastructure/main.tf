5c3ec63d5cae617968a6119d4ccf6f1c90e37af2
Vinh Le <vinh.ninja@icloud.com>
2018-05-16 00:34:08 +0800

    Grant all s3 actions to lambda

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -64,10 +64,7 @@ resource "aws_iam_role_policy" "fm_storage" {
   "Statement": [
     {
       "Action": [
-        "s3:GetObject",
-        "s3:GetObjectVersion",
-        "s3:PutObject",
-        "s3:PutObjectAcl"
+        "s3:*"
       ],
       "Effect": "Allow",
       "Resource": "${aws_s3_bucket.storage.arn}/*"
